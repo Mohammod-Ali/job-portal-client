5934ce864afa662d53599671f117c3a2b9c53870
Mohammod Ali <mohammodali651@gmail.com>
2025-06-18 23:54:44 +0600

    email and password authentication added

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,14 +1,30 @@
 import Lottie from "lottie-react";
 import registerLottieData from '../../assets/lottie/register.json'
+import { useContext } from "react";
+import AuthContext from "../../context/AuthContext/AuthContext";
 
 const Register = () => {
-    
+  
+  const {createUser} = useContext(AuthContext)
+
     const handleRegister= e => {
         e.preventDefault()
         const form = e.target
         const email = form.email.value
         const password = form.password.value
-        console.log(email, password)
+        // console.log(email, password)
+       
+        // password authentication
+
+
+        // check authentication
+        createUser(email, password)
+        .then( result => {
+          console.log(result.user)
+        })
+        .catch(error => {
+          console.log(error.message)
+        })
     }
     return (
         <div className="hero bg-base-200 min-h-screen">
