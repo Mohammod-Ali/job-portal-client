0b35057ff2e05dce18bdfb0a55e6ea1d36db4e37
Mohammod Ali <mohammodali651@gmail.com>
2025-06-30 18:48:32 +0600

    jwt token used and axios base url used

---
+++ src/hooks/useJobs.jsx
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from 'react';
+
+const useJobs = () => {
+    const [jobs, useJobs] = useState([])
+
+    useEffect( () => {
+
+    },[])
+    
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default useJobs;
